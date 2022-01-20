c0b47ce5b76df1202fb7b1c091535f955ab0f615
unknown <tri.worawat@gmail.com>
2022-01-20 22:08:04 +0700

    Disable Versionkey

--- model/Books.js
+++ model/Books.js
@@ -1,46 +1,59 @@
 const mongoose = require("mongoose");
 
-const bookShcema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const bookShcema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  desc: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
-const rentSchema = new mongoose.Schema({
-  bookId: {
-    type: String,
-    required: true,
+const rentSchema = new mongoose.Schema(
+  {
+    bookId: {
+      type: String,
+      // required: true,
+    },
+    bookTitle: {
+      type: String,
+      // required: true,
+    },
+    userId: {
+      type: String,
+      // requiure: true,
+    },
+    rentAmount: {
+      type: Number,
+      // required: true,
+    },
+    status: {
+      type: String,
+      // required: true,
+    },
+    rentDate: {
+      type: Date,
+      // default: Date.now,
+    },
+    returnAt: {
+      type: Date,
+    },
   },
-  bookTitle: {
-    type: String,
-    required: true,
-  },
-  userId: {
-    type: String,
-    requiure: true,
-  },
-  rentAmount: {
-    type: Number,
-    required: true,
-  },
-  rentDate: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 const Books = mongoose.model("Books", bookShcema);
 const rentBook = mongoose.model("rentBook", rentSchema);
