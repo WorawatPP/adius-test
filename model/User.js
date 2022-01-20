aaff840aea0b53e5af95465435a4bd5447a528fa
unknown <tri.worawat@gmail.com>
2022-01-20 22:07:36 +0700

    Disable Versionkey

--- model/User.js
+++ model/User.js
@@ -1,28 +1,31 @@
 const mongoose = require("mongoose");
 
-const userShcema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
+const userShcema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    email: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 255,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
-  },
-  password: {
-    type: String,
-    required: true,
-    min: 6,
-    max: 255,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-});
+  { versionKey: false }
+);
 
 module.exports = mongoose.model("User", userShcema);
